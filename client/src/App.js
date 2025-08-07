e9cde615531ec5278d94bd56914881a94a1e5bae
Gaethan <gaethan23@gmail.com>
2025-08-07 00:44:55 -0500

    Initialization

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>🏡 Welcome to the Homegoods Store!</h1>
+      <p>Browse our selection of quality home products.</p>
+    </div>
+  );
+}
